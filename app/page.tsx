07a7d61f88c3ca5f63c676e84ea661feeb397598
Nikita Gabetc <nikita.gabetc@gmail.com>
2025-11-14 01:01:12 +0300

    implement main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
+import EventCard from "@/components/EventCard"
+import ExploreBtn from "@/components/ExploreBtn"
+import { events } from "@/lib/constants"
+
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <section>
+            <h1 className="text-center">
+                The Hub for Every Dev <br /> Event You Can&apos;t Miss
+            </h1>
+            <p className="text-center mt-5">
+                Hackathons, Meetups, and Conferences, All in One Place
+            </p>
+            <ExploreBtn />
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+                <ul className="events">
+                    {events.map((item) => (
+                        <li key={item.title}>
+                            <EventCard {...item} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
 export default Home
